508648e47c3fa0a86bafcb51c3b01b417febe8f1
Richard-Alves167 <riichardd.08@gmail.com>
2025-11-13 21:59:48 -0300

    FEAT: adicionada uma classe 'Cliente'

--- Caixa_administrador/models.py
+++ Caixa_administrador/models.py
@@ -6,7 +6,7 @@ class Produto(declarative_base()):
 
     __tablename__ = "produto"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id_produto = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String, nullable=False)
     quantidade = Column(Integer, nullable=False)
     preco = Column(Float, nullable=False)
@@ -18,3 +18,17 @@ class Produto(declarative_base()):
 
     def __str__(self):
         return f"{self.id},{self.nome},{self.quantidade},{self.preco}"
+    
+class Cliente(declarative_base()):
+    '''Classe que representa um cliente com ID e nome.'''
+
+    __tablename__ = "cliente"
+
+    id_cliente = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String, nullable=False)
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def __str__(self):
+        return f"{self.id},{self.nome}"
